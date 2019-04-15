d171aa209212aa628a47d4d3f30208f3b354edf3
Erio <lolierio@qq.com>
2019-04-15 15:34:31 +0800

    Project initialization.

---
+++ src/main/java/com/erio/wecord/WecordApplication.java
@@ -0,0 +1,13 @@
+package com.erio.wecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecordApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WecordApplication.class, args);
+  }
+
+}
